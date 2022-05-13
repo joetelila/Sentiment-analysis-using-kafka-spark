47a4d878f22854604bcf2856265522cac9468086
Yohannis K Telila <joey.kifle@gmail.com>
2022-05-13 17:43:58 +0200

    add comments on dataframes

--- kafka-script/tweet_consumer.py
+++ kafka-script/tweet_consumer.py
@@ -9,15 +9,15 @@ from pyspark import SparkConf
 import pyspark.sql.functions as F
 import requests
 
-# url for updating result.
 sentiments = ["Neutral", "Positive", "Negative"]
 sentiment_counts = {'Neutral':0, 'Positive':0, 'Negative':0}
+# url for updating result.
 url = 'http://localhost:5000/updatedata'
-
 
 conf = SparkConf()
 conf.set("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.2.1")
-                 
+# johnsnowlab nlp package
+conf.set("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:3.4.4")
 #spark_master = "spark://131.114.50.200:7077"
 #spark_master = "spark://joetelila.local:7077"
 #spark_master = "spark://joetelila.lan:7077"
@@ -34,7 +34,7 @@ spark = SparkSession\
 print("[INFO] : Loading model...")
 pipeline_model = PipelineModel.load('pipeline_lr_model')
 print("[INFO] : Model loaded successfully")
-
+print("[INFO] : This model has accuracy of - 60%, HODL on better model is training . . .")
 
 kafka_server = "131.114.50.200:9092"
 #kafka_server = "localhost:9092"
@@ -47,7 +47,16 @@ df = spark \
   .option("startingOffsets", "latest") \
   .load()
 df.printSchema()
-
+'''
+root
+ |-- key: binary (nullable = true)
+ |-- value: binary (nullable = true)
+ |-- topic: string (nullable = true)
+ |-- partition: integer (nullable = true)
+ |-- offset: long (nullable = true)
+ |-- timestamp: timestamp (nullable = true)
+ |-- timestampType: integer (nullable = true)
+'''
 # we are interested in the value column since it contains all the information from the kafka stream
 # By default the value column is byte array, so we need to cast it to string format.
 tweet_value = df.selectExpr("CAST(value AS STRING)")
@@ -55,22 +64,39 @@ tweet_schema = StructType() \
                .add("created_at", StringType())\
                .add("text", StringType()) \
                .add("lang", StringType()) \
-                .add("author_id", StringType())
+               .add("author_id", StringType())
 
 tweet_df = tweet_value.select(from_json(col("value"), tweet_schema).alias("tweet"))
 tweet_df.printSchema()
-
-# make a pridiction on each tweet of the stream
+'''
+root
+ |-- tweet: struct (nullable = true)
+ |    |-- created_at: string (nullable = true)
+ |    |-- text: string (nullable = true)
+ |    |-- lang: string (nullable = true)
+ |    |-- author_id: string (nullable = true)
+'''
+# cleaning up tweets
 tweet_df = tweet_df.select("tweet.created_at", "tweet.text", "tweet.lang", "tweet.author_id")
+tweet_df.printSchema()
+'''
+root
+ |-- created_at: string (nullable = true)
+ |-- text: string (nullable = true)
+ |-- lang: string (nullable = true)
+ |-- author_id: string (nullable = true)
+'''
+# remove handle from the tweets
 tweet_df = tweet_df.withColumn('text', F.regexp_replace('text','@[A-Za-z0-9_]+',''))
+# remove links from the tweets
 tweet_df = tweet_df.withColumn('text', F.regexp_replace('text','https?://[^ ]+',''))
 tweet_df = tweet_df.withColumn('text', F.regexp_replace('text','www.[^ ]+',''))
+
 #tweet_df = pipelineFit.transform(tweet_df)
 pred_tweet= pipeline_model.transform(tweet_df).select('author_id','text','probability','prediction')
 
-# Group (the count) tweets by prediction. 
+# Group (the count) tweets by prediction.
 twt_pred = pred_tweet.groupBy('prediction').count()
-
 
 def prepare_request_data(data_dict):
         global sentiment_counts, sentiments
